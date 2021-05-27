822b9f8f76d9e21742954dbd4a843aec3fc23d15
大塚　碧人 <32205@ojs.ac.jp>
2021-05-27 14:41:17 +0900

    P64 リスト2.17

---
+++ SalesCalculator/SalesCalculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesCalculator {
+    //売り上げ集計クラス
+    class SalesCalculator 
+    {
+        private List<Sale> _sales;
+
+        //コンストラクタ
+        public SalesCalculator(List<Sale> sales) 
+        {
+            _sales = sales;
+        }
+
+        //List 2-17
+        //店舗別売り上げを求める
+        public Dictionary<string,int> GetPerStoreSales()
+        {
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            foreach(Sale sale in _sales) 
+            {
+                if (dict.ContainsKey(sale.ShopName))
+                    dict[sale.ShopName] += sale.Amount;
+                else
+                    dict[sale.ShopName] = sale.Amount;
+            }
+            return dict;
+        }
+    }
+}
